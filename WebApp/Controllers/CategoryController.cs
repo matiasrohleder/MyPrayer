982a2f449ccb87947ad5e189fabfdb24805fac30
Camila <camila.guardia@hotmail.com>
2024-02-26 19:21:23 -0300

    Category ABM

--- WebApp/Controllers/CategoryController.cs
+++ WebApp/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 using DataLayer.Interfaces;
 using Entities.Models;
 using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
 
 namespace WebApp.Controllers;
 
@@ -13,5 +14,49 @@ public class CategoryController(
     public IActionResult Index()
     {
         return View();
+    }
+
+    public IActionResult Create()
+    {
+        ViewData["Action"] = "Create";
+        return View("CreateOrEdit", new CategoryViewModel());
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CategoryViewModel category)
+    {
+        if (ModelState.IsValid)
+        {
+            await categoryService.AddAsync(category.ToEntity());
+            return RedirectToAction("Index", "Category");
+        }
+
+        ViewData["Action"] = "Create";
+
+        return View("CreateOrEdit", category);
+    }
+
+    public async Task<IActionResult> Edit(Guid id)
+    {
+        Category category = await categoryService.GetAsync(id);
+        CategoryViewModel categoryViewModel = new CategoryViewModel(category);
+
+        ViewData["Action"] = "Edit";
+
+        return View("CreateOrEdit", categoryViewModel);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(CategoryViewModel category)
+    {
+        if (ModelState.IsValid)
+        {
+            await categoryService.UpdateAsync(category.ToEntity());
+            return RedirectToAction("Index", "Category");
+        }
+
+        ViewData["Action"] = "Edit";
+
+        return View("CreateOrEdit", category);
     }
 }
