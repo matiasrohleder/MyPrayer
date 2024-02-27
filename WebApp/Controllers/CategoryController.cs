97dd06c3d457553034839039d451d37cc015e174
Camila <camila.guardia@hotmail.com>
2024-02-27 10:52:49 -0300

    Delete + SWAL

--- WebApp/Controllers/CategoryController.cs
+++ WebApp/Controllers/CategoryController.cs
@@ -6,10 +6,12 @@ using WebApp.Models;
 namespace WebApp.Controllers;
 
 public class CategoryController(
-    IService<Category> categoryService
+    IService<Category> categoryService,
+    IService<Content> contentService
     ) : Controller
 {
     private readonly IService<Category> categoryService = categoryService;
+    private readonly IService<Content> contentService = contentService;
 
     public IActionResult Index()
     {
@@ -25,7 +27,7 @@ public class CategoryController(
     [HttpGet]
     public IActionResult GetAll()
     {
-        List<CategoryViewModel> categories = categoryService.GetAll().Select(c => new CategoryViewModel(c)).ToList();
+        List<CategoryViewModel> categories = categoryService.GetAll().Where(c => !c.Deleted).Select(c => new CategoryViewModel(c)).ToList();
 
         return Json(new { data = categories });
     }
@@ -66,5 +68,18 @@ public class CategoryController(
         ViewData["Action"] = "Edit";
 
         return View("CreateOrEdit", category);
+    }
+
+    public async Task<JsonResult> Delete(Guid id)
+    {
+        bool categoryInUse = contentService.GetAll().Any(c => c.CategoryId == id);
+
+        if (!categoryInUse)
+        {
+            await categoryService.DeleteAsync(id);
+            return new JsonResult(null);
+        }
+        else
+            throw new Exception();
     }
 }
--- WebApp/wwwroot/js/views/category/index.js
+++ WebApp/wwwroot/js/views/category/index.js
@@ -23,8 +23,9 @@ $(document).ready(function () {
                 },
             },
             {
+                orderable: false,
                 "render": function (data, type, full, meta) {
-                    return '<button class="btn btn-info" href="/Category/Edit/' + full.id + '">Editar</button>';
+                    return `<button class="btn btn-info" style="margin-right:1em" href="/Category/Edit/${full.id}">Editar</button><button class="btn btn-cancel" onclick="Delete('${full.id}')">Borrar</button>`;
                 }
             },
         ],
@@ -33,3 +34,27 @@ $(document).ready(function () {
         },
     });
 });
+
+function Delete(id)
+{
+    $.ajax({
+        url: `/Category/Delete/${id}`,
+        type: 'GET',
+        success: function (result) {
+            Swal.fire({
+                title: "\xc9xito",
+                text: "Categor\xeda eliminada",
+                type: "success"
+            }).then((result) => {
+                window.location.reload();
+            });
+        },
+        error: function () {
+            Swal.fire({
+                title: "No es posible eliminar",
+                text: "Hay contenidos pertenecientes a esta categor\xeda",
+                type: "error"
+            });
+        }
+    });
+}
