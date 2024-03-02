befbfa5905c8b2fa3114d29bda01209da1796193
Camila <camila.guardia@hotmail.com>
2024-03-02 12:30:45 -0300

    get recent

---
+++ WebAPI/Controllers/ContentController.cs
@@ -0,0 +1,47 @@
+﻿using DataLayer.Interfaces;
+using Entities.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.DTOs;
+
+namespace WebAPI.Controllers
+{
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContentController : Controller
+    {
+        private readonly IService<Content> contentService;
+
+        public ContentController(IService<Content> contentService)
+        {
+            this.contentService = contentService;
+        }
+
+        [HttpGet]
+        public IActionResult GetRecent(int amount = 5)
+        {
+            List<RecentContentItem> recents = contentService.GetAll()
+                                                            .Include(c => c.Category)
+                                                            .Where(c => !c.Deleted && c.Active)
+                                                            .GroupBy(c => c.CategoryId)
+                                                            .Select(c => new RecentContentItem(c.First().Category)
+                                                            {
+                                                                Contents = c.OrderByDescending(i => i.ShowDate).Take(amount).Select(i => new RecentContentDTO(i)).ToList()
+                                                            })
+                                                            .ToList()
+                                                            .OrderBy(c => c.Order)
+                                                            .ToList();
+
+            int count = recents.Count();
+            return Ok(new
+            {
+                Total = count,
+                Page = 1,
+                PageSize = count,
+                Items = recents
+            });
+        }
+    }
+}
---
+++ WebAPI/DTOs/RecentContentItem.cs
@@ -0,0 +1,38 @@
+﻿using Entities.Models;
+
+namespace WebAPI.DTOs
+{
+    public class RecentContentItem
+    {
+        public string Category { get; set; }
+        public Guid CategoryId { get; set; }
+        public int Order { get; set; }
+        public List<RecentContentDTO> Contents { get; set; }
+
+        public RecentContentItem(Category category)
+        {
+            Category = category.Name;
+            CategoryId = category.Id;
+            Order = category.Order;
+            Contents = new List<RecentContentDTO>();
+        }
+    }
+
+    public class RecentContentDTO
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime ShowDate { get; set; }
+        public string Link { get; set; }
+        
+        // TO-DO: add image
+
+        public RecentContentDTO(Content content)
+        {
+            Name = content.Name;
+            Description = content.Description;
+            ShowDate = content.ShowDate;
+            Link = content.Link;
+        }
+    }
+}
