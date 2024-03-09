e1332c9cae23bc401517655f350764108e554bf2
camiguardia <156109307+camiguardia@users.noreply.github.com>
2024-03-09 15:27:29 -0300

    Api - Content Endpoints (#24)
    * Model changes
    
    * web api configurations
    
    * API Key Authentication
    
    * Add condition
    
    * get by category and by id endpoints

---
+++ WebAPI/Controllers/ContentController.cs
@@ -0,0 +1,60 @@
+﻿using DataLayer.Interfaces;
+using Entities.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.DTOs;
+
+namespace WebAPI.Controllers
+{
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContentController : Controller
+    {
+        private readonly IService<Content> contentService;
+
+        public ContentController(IService<Content> contentService)
+        {
+            this.contentService = contentService;
+        }
+
+        [HttpGet("getRecent")]
+        public IActionResult GetRecent(int amount = 5)
+        {
+            List<RecentContentItem> recents = contentService.GetAll()
+                                                            .Include(c => c.Category)
+                                                            .Where(c => !c.Deleted && c.Active && c.ShowDate <= DateTime.Now)
+                                                            .GroupBy(c => c.CategoryId)
+                                                            .Select(c => new RecentContentItem(c.First().Category)
+                                                            {
+                                                                Contents = c.OrderByDescending(i => i.ShowDate).Take(amount).Select(i => new ContentDTO(i)).ToList()
+                                                            })
+                                                            .ToList()
+                                                            .OrderBy(c => c.Order)
+                                                            .ToList();
+
+            return Ok(recents);
+        }
+
+        [HttpGet("getByCategory")]
+        public IActionResult GetByCategory(Guid categoryId)
+        {
+            List<ContentDTO> contents = contentService.GetAll()
+                                                            .Where(c => !c.Deleted && c.Active && c.ShowDate <= DateTime.Now && c.CategoryId == categoryId)
+                                                            .OrderByDescending(c => c.ShowDate)
+                                                            .Select(c => new ContentDTO(c))
+                                                            .ToList();
+
+            return Ok(contents);
+        }
+
+        [HttpGet("get")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            Content content = await contentService.GetAsync(id);
+
+            return Ok(new ContentDTO(content));
+        }
+    }
+}
---
+++ WebAPI/DTOs/RecentContentItem.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Models;
+
+namespace WebAPI.DTOs
+{
+    public class RecentContentItem
+    {
+        public string Category { get; set; }
+        public Guid CategoryId { get; set; }
+        public int Order { get; set; }
+        public List<ContentDTO> Contents { get; set; }
+
+        public RecentContentItem(Category category)
+        {
+            Category = category.Name;
+            CategoryId = category.Id;
+            Order = category.Order;
+            Contents = new List<ContentDTO>();
+        }
+    }
+}
