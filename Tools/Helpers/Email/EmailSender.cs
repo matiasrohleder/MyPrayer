0431089c75c11735260665c91e56c4870b85f902
Matias D. Rohleder <matiasrohleder@gmail.com>
2024-03-15 19:16:19 -0300

    Remove brackets from tools

--- Tools/Helpers/Email/EmailSender.cs
+++ Tools/Helpers/Email/EmailSender.cs
@@ -16,7 +16,7 @@ public class EmailSender(
         ) : IEmailSender
 {
     #region Builder Properties
-    private List<string> Emails { get; set; } = [];
+    private List<string> Emails { get; set; } = new();
     private Func<string> SubjectProvider { get; set; } = () => "";
     private Func<IEmailDTO> BodyTemplateModelProvider { get; set; } = () => null!;
     private Func<string> HtmlBodyProvider { get; set; } = () => "";
