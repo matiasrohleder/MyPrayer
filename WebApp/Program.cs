f15fa892d639bdb37f3da6d4d43dee36ebc41be9
Fernando Kabas <fkabas@binit.dev>
2024-03-29 18:36:37 +0100

    Use configure kestrel

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -22,10 +22,15 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole<Guid>>(config =>
     .AddDefaultTokenProviders();
 #endregion
 
-var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-//var sslPort = Environment.GetEnvironmentVariable("SSL_PORT") ?? "8081";
-//builder.WebHost.UseUrls($"http://*:{port}", $"https://*:{sslPort}");
-builder.WebHost.UseUrls($"http://0.0.0.0:5000");
+// var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+// //var sslPort = Environment.GetEnvironmentVariable("SSL_PORT") ?? "8081";
+// //builder.WebHost.UseUrls($"http://*:{port}", $"https://*:{sslPort}");
+// builder.WebHost.UseUrls($"http://0.0.0.0:5000");
+
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+    serverOptions.Listen(System.Net.IPAddress.Any, 5000); // Listen on all network interfaces on port 5000
+});
 
 var app = builder.Build();
 
