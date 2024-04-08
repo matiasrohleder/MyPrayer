89edc06b2a81504b4d482a5370b9991411fb91bc
Matias D. Rohleder <matiasrohleder@gmail.com>
2024-04-07 22:22:10 -0300

    SeedManager and EntitySeeds

--- Entities/Constants/Authentication/Roles.cs
+++ Entities/Constants/Authentication/Roles.cs
@@ -5,7 +5,7 @@ namespace Entities.Constants.Authentication;
 public static class Roles
 {
     public const string Admin = "Admin";
-    public const string User = "User";
+    private static readonly Guid adminId = Guid.Parse("3f3dd5b3-b480-4ea2-a477-101010101010");
 
     public static ICollection<string> GetAllRoles() =>
         typeof(Roles)
@@ -13,4 +13,18 @@ public static class Roles
         .Where(f => f.IsLiteral && !f.IsInitOnly)
         .Select(f => (f.GetValue(null) as string) ?? string.Empty)
         .ToList();
+
+    /// <summary>
+    /// Get the id from the current role
+    /// </summary>
+    /// <param name="role">Name of the role you want to get the id</param>
+    public static Guid GetId(string role)
+    {
+        FieldInfo[] publicFields = typeof(Roles).GetFields(BindingFlags.Static | BindingFlags.Public);
+        string rolName = publicFields.First(f => f.GetValue(null).ToString() == role).Name;
+
+        string idFieldName = $"{char.ToLowerInvariant(rolName[0])}{rolName[1..]}Id";
+        FieldInfo[] privateFields = typeof(Roles).GetFields(BindingFlags.Static | BindingFlags.NonPublic);
+        return Guid.Parse(privateFields.First(f => f.Name == idFieldName).GetValue(null).ToString());
+    }
 }
--- README.md
+++ README.md
@@ -3,12 +3,12 @@
 ## PostgreSQL
 ### Create
 ```sh
-dotnet ef migrations add Initial --startup-project ..\WebApp\WebApp.csproj --context ModelsDbPostgreSQLContext --output-dir Migrations\ModelsDbPostgreSQL
+dotnet ef migrations add Initial --startup-project ..\WebApp\WebApp.csproj --context ModelsDbContextPostgreSQL --output-dir Migrations\ModelsDbPostgreSQL
 ```
 
 ### Update
 ```sh
-dotnet ef database update --startup-project ..\WebApp\WebApp.csproj --context ModelsDbPostgreSQLContext
+dotnet ef database update --startup-project ..\WebApp\WebApp.csproj --context ModelsDbContextPostgreSQL
 ```
 
 ## SQLServer
