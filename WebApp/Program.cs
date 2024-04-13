e195cd868a80af44c6e90f9ea7fb55fb656a95dd
camiguardia <156109307+camiguardia@users.noreply.github.com>
2024-04-13 14:54:09 -0300

    WebApp Layout (#40)
    * layout
    
    * undo change
    
    * undo changes
    
    * modifications

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -42,6 +42,9 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+
+    // add razon runtime compilation
+    builder.Services.AddMvc().AddRazorRuntimeCompilation();
 }
 
 app.UseStaticFiles();
