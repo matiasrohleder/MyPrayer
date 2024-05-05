2472259c19cf4114ac478215240b45a9c993afbf
camiguardia <156109307+camiguardia@users.noreply.github.com>
2024-05-05 18:58:02 -0300

    add loading modal (#64)

--- WebApp/wwwroot/js/site.js
+++ WebApp/wwwroot/js/site.js
@@ -1,4 +1,9 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿function ShowLoadingModal() {
+    swal({
+        allowEscapeKey: false,
+        allowOutsideClick: false,
+        html: '<img src="/images/loading-gif.gif" style="width:100px">',
+        showConfirmButton: false,
+        width: '200px'
+    });
+}
