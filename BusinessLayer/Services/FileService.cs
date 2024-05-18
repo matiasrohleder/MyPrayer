d93d41c95016c8ad844ed84d127314217ffe2c54
fekabas <fernandokabas@gmail.com>
2024-05-18 15:59:45 +0200

    Add multiple storage capabilities (#71)
    Co-authored-by: Fernando Kabas <fkabas@binit.dev>

--- BusinessLayer/Services/FileService.cs
+++ BusinessLayer/Services/FileService.cs
@@ -1,5 +1,6 @@
 using BusinessLayer.Interfaces;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.Extensions.Configuration;
 
 namespace BusinessLayer.Services
@@ -7,11 +8,13 @@ namespace BusinessLayer.Services
     public class FileService : IFileService
     {
         private readonly IConfiguration configuration;
-        private readonly IHttpClientFactory clientFactory;
-        private string apiKey => configuration.GetSection("FileService:APIKey").Value ?? throw new Exception("File Service API key must have a value");
-        private string baseURL => this.configuration.GetSection("FileService:BaseURL").Value ?? throw new Exception("File Service BaseURL must have a value");
+        private string apiKey => this.configuration.GetSection("FileService:APIKey")?.Value ?? throw new Exception("File Service API key must have a value");
+        private string baseURL => this.configuration.GetSection("FileService:BaseURL")?.Value ?? throw new Exception("File Service BaseURL must have a value");
+        private string imageBucket => this.configuration.GetSection("FileService:ImageBucket")?.Value ?? throw new Exception("File Service ImageBucket must have a value");
+        private string audioBucket => this.configuration.GetSection("FileService:AudioBucket")?.Value ?? throw new Exception("File Service AudioBucket must have a value");
+        private string behaviourOnBucketMissing => this.configuration.GetSection("FileService:BehaviourOnBucketMissing")?.Value ?? throw new Exception("File Service BehaviourOnBucketMissing must have a value");
 
-        private async Task<Supabase.Storage.Interfaces.IStorageFileApi<Supabase.Storage.FileObject>> GetStorage()
+        private async Task<Supabase.Storage.Interfaces.IStorageFileApi<Supabase.Storage.FileObject>> GetStorage(string bucket = "TEST")
         {
             var url = this.baseURL;
             var key = this.apiKey;
@@ -24,13 +27,21 @@ namespace BusinessLayer.Services
             var supabase = new Supabase.Client(url, key, options);
             await supabase.InitializeAsync();
 
-            return supabase.Storage.From("TEST");
+            var bucketList = await supabase.Storage.ListBuckets();
+            if(bucketList is null || bucketList.Count == 0 || !bucketList.Any(bl => bl.Name == bucket))
+            {
+                if(!string.IsNullOrEmpty(behaviourOnBucketMissing) && behaviourOnBucketMissing == "Create")
+                    await supabase.Storage.CreateBucket(bucket);
+                else // TODO: Explicitly check for "Exception" or "Throw" string constant value. Also group these possible values in an Enum or static class.
+                    throw new Exception($"There is no bucket named {bucket}. Please, make sure  your Supabase instance is configured with a {bucket} Bucket or check if the spelling is correct in the appsettings or environment variables for this configuration.");
+            }
+
+            return supabase.Storage.From(bucket);
         }
 
-        public FileService(IConfiguration configuration, IHttpClientFactory clientFactory)
+        public FileService(IConfiguration configuration)
         {
             this.configuration = configuration;
-            this.clientFactory = clientFactory;
         }
 
         /// <inheritdoc />
@@ -38,8 +49,10 @@ namespace BusinessLayer.Services
         {
             if (file == null || file.Length == 0)
                 throw new Exception("No file uploaded.");
-
-            var storage = await GetStorage();
+            
+            var storage = file.ContentType.Contains("image") ? await GetStorage(imageBucket)
+                        : file.ContentType.Contains("audio") ? await GetStorage(audioBucket)
+                                                             : await GetStorage();
 
             // Convert IFormFile to byte array
             byte[] fileBytes;
@@ -54,7 +67,7 @@ namespace BusinessLayer.Services
             string fileName = $"{now}-{file.FileName}";
 
             // Upload the file
-            string fileUrl = await storage.Upload(fileBytes, fileName);
+            await storage.Upload(fileBytes, fileName);
 
             return new FileUploadRes { FileUrl = fileName };
         }
@@ -62,10 +75,22 @@ namespace BusinessLayer.Services
         /// <inheritdoc />
         public async Task<FileDownloadRes> GetSignedURLAsync(string fileName)
         {
-            var storage = await GetStorage();
+            var provider = new FileExtensionContentTypeProvider();
 
-            // Generate a signed URL valid for 60 minutes
-            var result = await storage.CreateSignedUrl(fileName, 60 * 60);
+            if (!provider.TryGetContentType(fileName, out string contentType))
+                contentType = "";
+
+            var storage = contentType.Contains("image") ? await GetStorage(imageBucket)
+                        : contentType.Contains("audio") ? await GetStorage(audioBucket)
+                                                             : await GetStorage();
+
+            string result = "";
+            var list = await storage.List(options: new Supabase.Storage.SearchOptions(){
+                Search = fileName
+            });
+            if(list != null && list.Count > 0 && list.Any(f => f.Name == fileName))
+                // Generate a signed URL valid for 60 minutes
+                result = await storage.CreateSignedUrl(fileName, 60 * 60);
 
             return new FileDownloadRes { SignedUrl = result };
         }
