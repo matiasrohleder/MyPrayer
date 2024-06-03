749f6b1fc27e5c04879151da753669d5cc793b20
camiguardia <156109307+camiguardia@users.noreply.github.com>
2024-06-03 05:23:03 -0300

    GuidedMeditation - API Endpoint (#83)
    * Model
    
    * Migration
    
    * ABM
    
    * Add validation
    
    * endpoint

---
+++ WebAPI/Controllers/GuidedMeditationController.cs
@@ -0,0 +1,53 @@
+﻿using BusinessLayer.Interfaces;
+using DataLayer.Interfaces;
+using Entities.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.DTOs;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/guided-meditation")]
+    [ApiController]
+    [Authorize]
+    public class GuidedMeditationController : Controller
+    {
+        private readonly IService<GuidedMeditation> guidedMeditationService;
+        private readonly IFileService fileService;
+
+        public GuidedMeditationController(IService<GuidedMeditation> guidedMeditationService, IFileService fileService)
+        {
+            this.guidedMeditationService = guidedMeditationService;
+            this.fileService = fileService;
+        }
+
+        /// <summary>
+        /// Get all contents by category id
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns></returns>
+        [HttpGet("current")]
+        public async Task<ActionResult<GuidedMeditationRes>> GetCurrentByDate(DateTime? date)
+        {
+            if (!date.HasValue)
+                date = DateTime.Now;
+
+            GuidedMeditation? guidedMeditation = await guidedMeditationService.GetAll()
+                                                                              .FirstOrDefaultAsync(gm => !gm.Deleted && 
+                                                                                                         gm.Active && 
+                                                                                                         gm.StartDate <= date.Value.ToUniversalTime() && 
+                                                                                                         gm.EndDate > date.Value.ToUniversalTime());
+            if (guidedMeditation == null)
+                return BadRequest("No hay meditaciones guiadas para la fecha.");
+            
+            GuidedMeditationRes guidedMeditationRes = new(guidedMeditation);
+
+            // Build public URLs for files
+            if (!string.IsNullOrEmpty(guidedMeditation.FileUrl))
+                guidedMeditationRes.Audio = (await fileService.GetSignedURLAsync(guidedMeditationRes.Audio)).SignedUrl;
+
+            return Ok(guidedMeditationRes);
+        }
+    }
+}
---
+++ WebAPI/DTOs/GuidedMeditationRes.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Models;
+
+namespace WebAPI.DTOs
+{
+    public class GuidedMeditationRes
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Audio { get; set; }
+        
+        public GuidedMeditationRes(GuidedMeditation guidedMeditation)
+        {
+            Name = guidedMeditation.Name;
+            Description = guidedMeditation.Description;
+            Id = guidedMeditation.Id.ToString();
+            StartDate = guidedMeditation.StartDate;
+            EndDate = guidedMeditation.EndDate;
+            Audio = !string.IsNullOrEmpty(guidedMeditation.FileUrl) ? guidedMeditation.FileUrl : string.Empty;
+        }
+    }
+}
