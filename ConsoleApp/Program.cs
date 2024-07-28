76b38b678dde8bf85db2de7582fa0682fb7ef437
Matias D. Rohleder <matiasrohleder@gmail.com>
2024-07-27 22:52:25 -0300

    new program

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -11,6 +11,7 @@ using Entities.Models.DbContexts;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using System.Net;
+using Entities.Helpers;
 
 namespace ConsoleApp;
 
@@ -18,30 +19,40 @@ class Program
 {
     static async Task Main(string[] args)
     {
-        // Set up dependency injection
-        // var serviceProvider = new ServiceCollection()
-        //     .AddSingleton<IConfiguration>(_ => new ConfigurationBuilder()
-        //     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-        //     .AddJsonFile($"appsettings.Development.json", optional: true)
-        //     .Build())
-        //     // .AddSingleton<IConfiguration>(x => new MyPrayerConfiguration(x.GetRequiredService<IWebHostEnvironment>(), x.GetRequiredService<IServiceProvider>()))
-        //     .AddScoped<IBibleConfiguration, BibleConfiguration>()
-        //     .AddTransient<IReadingBusinessLogic, ReadingBusinessLogic>() // Register ReadingBusinessLogic
-        //     .AddTransient<IService<Reading>, Service<Reading>>() // Register Service<Reading>
-        //     .AddDbContext<ModelsDbContext>()  // Register ModelsDbContext
-        //     .AddScoped<IRepositoryFactory, RepositoryFactory>() // Register RepositoryFactory
-        //     .AddScoped<IUnitOfWork>((provider) =>
-        //     {
-        //         ModelsDbContext modelDbContext = provider.GetRequiredService<ModelsDbContext>();
-        //         IRepositoryFactory repositoryFactory = provider.GetRequiredService<IRepositoryFactory>();
-        //         return new UnitOfWork(modelDbContext, repositoryFactory);
-        //     })
-        //     .AddHttpClient()
-        //     .BuildServiceProvider();
+        // Create the service collection
+        var services = new ServiceCollection();
 
-        // // Resolve the service and call the method
-        // var readingBusinessLogic = serviceProvider.GetRequiredService<IReadingBusinessLogic>();
-        // await readingBusinessLogic.GetReadings();
+        // Set up dependency injection
+        services
+            .AddSingleton<IConfiguration>(_ => new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.Development.json", optional: true)
+            .Build())
+            // .AddSingleton<IConfiguration>(x => new MyPrayerConfiguration(x.GetRequiredService<IWebHostEnvironment>(), x.GetRequiredService<IServiceProvider>()))
+            .AddScoped<IBibleConfiguration, BibleConfiguration>()
+            .AddTransient<IReadingBusinessLogic, ReadingBusinessLogic>() // Register ReadingBusinessLogic
+            .AddTransient<IService<Reading>, Service<Reading>>() // Register Service<Reading>
+            .AddDbContext<ModelsDbContext>()  // Register ModelsDbContext
+            .AddScoped<IRepositoryFactory, RepositoryFactory>() // Register RepositoryFactory
+
+            // Register unit of work
+            .AddScoped<IUnitOfWork>((provider) =>
+            {
+                ModelsDbContext modelDbContext = provider.GetRequiredService<ModelsDbContext>();
+                IRepositoryFactory repositoryFactory = provider.GetRequiredService<IRepositoryFactory>();
+                return new UnitOfWork(modelDbContext, repositoryFactory);
+            })
+            .AddHttpClient();
+
+        // Register databases
+        DbContextFactoryHelper.RegisterModelsDbContext(services);
+
+        // Build the service provider
+        var serviceProvider = services.BuildServiceProvider();
+
+        // Resolve the service and call the method
+        var readingBusinessLogic = serviceProvider.GetRequiredService<IReadingBusinessLogic>();
+        await readingBusinessLogic.GetReadings();
 
         Console.WriteLine("Job executed successfully.");
     }
